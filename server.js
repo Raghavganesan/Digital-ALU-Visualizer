82526560f28ed5f3bcc87940bebda2ea49fd615c
Raghavganesan <riogunismain@gmail.com>
2025-10-26 14:34:44 +0530

    new

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import { exec } from "child_process";
 
 const app = express();
 app.use(express.json());
-app.use(express.static("."));
+app.use(express.static("public"));
 
 const selMap = {
   "000": "Addition",
